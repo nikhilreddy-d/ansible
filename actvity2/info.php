a93d6ed8dd67a6281e6717a5d0e1284d3047ab3b
nikhilreddy-d <doddanikhil2001@gmail.com>
2024-03-10 18:53:56 +0530

    added activity2

--- actvity2/info.php
+++ actvity2/info.php
@@ -1,3 +1,4 @@
-### file that need for the playbook
+### file that need for the playbook 
+### file that is present in the remote server or the remoteabsloute path for the php 
 
 <?php phpinfo(); ?>
